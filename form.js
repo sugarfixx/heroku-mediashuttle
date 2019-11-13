682acff841200ddd96ce44348476b195522694b5
sugarfixx <ingar.torsrud@me.com>
2019-11-13 23:34:07 +0100

    Revert to github hosted form

--- form.js
+++ form.js
@@ -13,7 +13,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const textParser = bodyParser.text({ type: '*/*' });
 
 let registrationKey = 'e80764b1-d79b-4c61-9914-eafc677cc793';
-let formUrl = 'https://mediashuttle.herokuapp.com/form.html';
+let formUrl = 'https://raw.githubusercontent.com/Signiant/mediashuttle-metadata-webtask/master/Basic-Form/form.html';
 
 const generateSignedUrl = (requestUrl, requestBody, registrationKey) => {
     const requestTimestamp = new Date().toISOString();
@@ -44,11 +44,11 @@ app.use('/show', urlencodedParser, function (req, res) {
   if (req.query.formUrl) {
     formUrl = req.query.formUrl;
   };
-  
+
   if (req.query.registrationKey) {
     registrationKey = req.query.registrationKey;
   }
-  
+
   rp.get(formUrl)
     .then(form => {
       res.send(require('ejs').render(form, {
