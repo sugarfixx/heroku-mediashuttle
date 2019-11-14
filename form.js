cf838aef87420baf3a4e79f0d15143fdfe420599
sugarfixx <ingar.torsrud@me.com>
2019-11-14 01:34:51 +0100

    Adds test route and refactirs actions

--- form.js
+++ form.js
@@ -1,19 +1,15 @@
-'use strict';
-
 const Express = require('express');
-const Webtask = require('webtask-tools');
 const app = Express();
 const bodyParser = require('body-parser');
 const querystring = require('querystring');
 const crypto = require('crypto');
-const rp = require('request-promise');
 const PORT = process.env.PORT || 5000
 
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const textParser = bodyParser.text({ type: '*/*' });
 
 let registrationKey = 'e80764b1-d79b-4c61-9914-eafc677cc793';
-let formUrl = 'https://raw.githubusercontent.com/Signiant/mediashuttle-metadata-webtask/master/Basic-Form/form.html';
+let formUrl = '/form.html';
 
 const generateSignedUrl = (requestUrl, requestBody, registrationKey) => {
     const requestTimestamp = new Date().toISOString();
@@ -39,24 +35,13 @@ const generateSignedUrl = (requestUrl, requestBody, registrationKey) => {
     const signatureParam = `X-Sig-Signature=${signature}`;
     return `${requestUrl}?${algorithmParam}&${dateParam}&${signatureParam}`;
 };
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
 
 app.use('/show', urlencodedParser, function (req, res) {
-  if (req.query.formUrl) {
-    formUrl = req.query.formUrl;
-  }
-  /*
-  if (req.query.registrationKey) {
-    registrationKey = req.query.registrationKey;
-  }
-  */
-  rp.get(formUrl)
-    .then(form => {
-      res.send(require('ejs').render(form, {
-        redirectUrl: req.body.redirectUrl
-      }));
-    })
-    .catch(err => {
-      res.status(500).send(err.message).end();
+    res.render( __dirname + formUrl, {
+        redirectUrl: req.body.redirectUrl,
+        packageId : req.body.packageId
     });
 });
 
@@ -66,8 +51,16 @@ app.post('/process', textParser, function (req, res) {
   res.set('Location', signedUrl);
   res.status(307).end();
 });
-app.listen(PORT, () => {
-  console.log("Server is listening on port: ${ PORT }");
+
+app.get("/", (req, res) => {
+    console.log( __dirname + formUrl);
+    res.render( __dirname + formUrl, {
+        redirectUrl: 'detta-gitt',
+        packageId : '123456789'
+    });
 });
 
-//  module.exports = Webtask.fromExpress(app);
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port:", PORT);
+});
